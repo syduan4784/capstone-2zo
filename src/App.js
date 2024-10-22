1c0f3bb6ca78a56fdab872d105a5b2e4e3abbc82
BuildTools <unconfigured@null.spigotmc.org>
2024-10-22 21:37:49 +0900

    Add Express backend to my-app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import Calendar from './pages/Calendar';
 import TextSearch from './pages/TextSearch';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // 서버 API에 데이터 요청
+    axios.get('http://[52.78.154.108]:[3000]/api/data')
+      .then(response => {
+        setData(response.data); // 서버에서 받은 데이터를 상태에 저장
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <Router>
       <Routes>
