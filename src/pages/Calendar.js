49edb817b7719b29535b902193abafd414692535
5645008 <kangbumsuck@gmail.com>
2024-10-24 13:21:08 +0900

    reset

--- src/pages/Calendar.js
+++
@@ -1,7 +0,0 @@
-// src/pages/Calendar.js
-
-const Calendar = () => {
-    return <div>Calendar Page</div>;
-  };
-  
-  export default Calendar;
--- src/pages/TextSearch.js
+++
@@ -1,7 +0,0 @@
-// src/pages/TextSearch.js
-
-const TextSearch = () => {
-    return <div>TextSearch Page</div>;
-  };
-  
-  export default TextSearch;
