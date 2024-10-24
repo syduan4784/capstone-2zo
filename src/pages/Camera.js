807bf5adee66d5b8d65b6dc0af4da4933a4deb64
5645008 <kangbumsuck@gmail.com>
2024-10-24 13:40:35 +0900

    10/24first-app-test

--- src/pages/Camera.js
+++
@@ -1,7 +0,0 @@
-// src/pages/Camera.js
-
-const Camera = () => {
-    return <div>Camera Page</div>;
-  };
-  
-  export default Camera;
--- src/pages/Glass.js
+++
@@ -1,7 +0,0 @@
-// src/pages/Glass.js
-
-const Glass = () => {
-    return <div>Glass Page</div>;
-  };
-  
-  export default Glass;
