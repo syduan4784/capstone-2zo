3898ecc0ead2089554fd1bfa781613e6f2b2733d
nguyen sy duan <syduan4784@gmail.com>
2024-11-12 14:17:48 +0900

    text scan component 추가

---
+++ src/pages/Camera.js
@@ -0,0 +1,26 @@
+// components/Camera.js
+import React, { useRef } from 'react';
+import Webcam from 'react-webcam';
+
+const Camera = ({ onCapture }) => {
+  const webcamRef = useRef(null);
+
+  const captureImage = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    onCapture(imageSrc);
+  };
+
+  return (
+    <div>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={300}
+      />
+      <button onClick={captureImage}>Capture Image</button>
+    </div>
+  );
+};
+
+export default Camera;
